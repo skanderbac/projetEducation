fad90d4386f31cfb7d32dedfd787cded9c0bf9db
skanderbac <skanderbac@users.noreply.github.com>
2021-09-09 23:06:31 +0100

    update matiere migration

--- database/migrations/2021_08_21_001824_create_matieres_table.php
+++ database/migrations/2021_08_21_001824_create_matieres_table.php
@@ -13,7 +13,7 @@ class CreateMatieresTable extends Migration
      */
     public function up()
     {
-        Schema::create('Matieres', function (Blueprint $table) {
+        Schema::table('Matieres', function (Blueprint $table) {
             $table->id();
             $table->string('nom');
             $table->string('type');
